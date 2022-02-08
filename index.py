9c856907506f33c7db8172109ef2ecb1ccd5ef18
BenitzCoding <benitzcoding@yahoo.com>
2022-02-08 16:38:41 +0800

    Added Content-Type upon returning, and fixed conditions.

--- index.py
+++ index.py
@@ -13,17 +13,41 @@ from functions import Constants, Generate
 
 app = FastAPI()
 
+def get_extension(path: str):
+	if os.path.is_file(path) == False:
+		return None
+	file = path.split("/")[-1]
+	extension = file.split(".")[-1]
+	return extension
+
+def get_clean_filename(path: str):
+	if os.path.is_file(path) == False:
+		return None
+	file = path.split("/")[-1]
+	filename = ""
+
+	seperated_filename_list = file.split(".")
+	for i in len(seperated_filename_list):
+		filename = filename + seperated_filename_list[i]
+		if i == (len(seperated_filename_list) - 1):
+			break
+
+	return filename
+
 class UploadPayload(BaseModel):
 	file: UploadFile = File(...)
+	format: str
 	name: Optional[str]
 
 @app.post("/upload")
 async def upload_file(payload: UploadPayload):
-	file = payload['file']
+	if format.startswith("."):
+		payload.format
+	file = payload.file
 	if Constants.get("ENABLE_CUSTOM_FILENAME") == False:
 		file.filename = Generate.token()
 
-	file.filename = f"{Generate.token()}.jpg"
+	file.filename = f"{Generate.token()}.{format}"
 	contents = await file.read()
 
 	with open(Constants.get("DIR") + str(file.filename), "wb") as f:
@@ -34,19 +58,16 @@ async def upload_file(payload: UploadPayload):
 		"url": f"{Constants.get('BASE_URL')}/{file.filename}"
 	}
 
-	return {"filename": file.filename}
+	return json.dumps(payload), 200, {'Content-Type': 'application/json'}
 
 @app.get("/{filename}")
 async def read_random_file(filename: str):
 	files = os.listdir(Constants.get("DIR"))
 	path = f"{Constants.get('DIR')}{files[filename]}"
-	if os.path.is_file(path) == False and os.path.is_file(path[:-3]) == False:
-		return json.dumps({ "404": "Not Found" }), 404
+	if os.path.is_file(path) == False:
+		return json.dumps({ "404": "Not Found" }), 404, {'Content-Type': 'application/json'}
 
-	elif os.path.is_file(path) == False:
-		path = path[:-3]
-
-	return FileResponse(path)
+	return FileResponse(path), 200
 
 if __name__ == '__main__':
-	uvicorn.run("main:app",host='127.0.0.1', port=80, reload=True, debug=True, workers=2)
+	uvicorn.run("main:app", host='127.0.0.1', port=80, reload=True, debug=True, workers=2)
